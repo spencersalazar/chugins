903187ef7a33efc4399d405e5a61b692b6e0387a
Spencer Salazar <spencer.salazar@gmail.com>
2014-08-10 18:12:01 -0700

    shutdown fix

--- MAUI/chuck_ui_mac.mm
+++ MAUI/chuck_ui_mac.mm
@@ -189,7 +189,7 @@ void Chuck_UI_Manager::shutdown()
 
 - (void)quit
 {
-    if(g_vm) g_vm->stop(); // termination of vm will cause application exit
+    if(g_vm) g_vm->shutdown(); // termination of vm will cause application exit
 }
 
 @end
